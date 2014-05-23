ef4e8d1c9dc91af8721e38f0370518d2152f2369
NielsKlop <niels_klop@hotmail.com>
2014-05-23 09:07:17 +0200

    Early weapons

---
+++ Assets/Resources/Weapons/Scripts/Bullet.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Bullet : MonoBehaviour {
+
+	private Transform myTransform;
+	public int currentSpeed = 20;
+
+	// Use this for initialization
+	void Start()
+	{
+		myTransform = transform;
+	}
+	
+	// Update is called once per frame
+	void Update()
+	{
+		myTransform.Translate(Vector3.right * currentSpeed * Time.deltaTime);
+
+		if (myTransform.position.x > 15)
+		{
+			DestroyObject(this.gameObject);
+		}
+	}
+}
---
+++ Assets/Resources/Weapons/Scripts/Rocket.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Rocket : MonoBehaviour {
+
+	private Transform myTransform;
+	public int currentSpeed = 7;
+
+	// Use this for initialization
+	void Start()
+	{
+		myTransform = transform;
+	}
+	
+	// Update is called once per frame
+	void Update()
+	{
+		myTransform.Translate(Vector3.right * currentSpeed * Time.deltaTime);
+	}
+}
