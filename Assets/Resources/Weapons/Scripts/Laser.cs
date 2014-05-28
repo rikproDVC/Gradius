c0098fdefb542468e56687e626ff55421165d1ab
NielsKlop <niels_klop@hotmail.com>
2014-05-28 09:43:16 +0200

    Weapons attached to player position and laser has charge.

--- Assets/Resources/Weapons/Scripts/Laser.cs
+++ Assets/Resources/Weapons/Scripts/Laser.cs
@@ -6,19 +6,35 @@ public class Laser : MonoBehaviour {
 	private Transform myTransform;
 	public int currentSpeed = 20;
 
+	private float LaserTimer;
+	private float ChargeTimer;
+	private Vector3 Position;
+
 	// Use this for initialization
 	void Start()
 	{
 		myTransform = transform;
+		LaserTimer = 0f;
+		ChargeTimer = 0f;
 	}
 	
 	// Update is called once per frame
 	void Update()
 	{
-		//myTransform.position = Player.position
-		if(Input.GetKeyUp(","))
+		//Position compared to player position
+		Position = Player.LaserPosition;
+		myTransform.position = Position;
+
+		//Timer to destroy
+		if(Player.LaserCharge == 0)
 		{
+			Player.LaserActive = false;
 			DestroyObject(this.gameObject);
+		}
+		if(Time.time - ChargeTimer > 0.05f && Player.LaserCharge > 0)
+		{
+			Player.LaserCharge -= 1;
+			ChargeTimer = Time.time;
 		}
 	}
 }
