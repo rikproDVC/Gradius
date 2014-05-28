37ca77cd88acff8f04efb9d6d768098171a993dc
jeremy balk <jeremy.balk@hotmail.com>
2014-05-28 10:24:11 +0200

    First level commit

---
+++ Assets/Levels/Scripts/MoveScript.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// Simply moves the current game object
+/// </summary>
+public class MoveScript : MonoBehaviour
+{
+	// 1 - Designer variables
+	
+	/// <summary>
+	/// Object speed
+	/// </summary>
+	public int xSpeed = 5;
+	
+	void Update()
+	{
+		transform.Translate(Vector3.left * xSpeed * Time.deltaTime);
+
+	}
+	
+	
+}
---
+++ Assets/Levels/Scripts/Spawn.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Spawn : MonoBehaviour {
+
+	public GameObject Meteorite;
+
+	public float Delay= 5F;
+	public float YMin = -4.9F;
+	public float YMax = 4.9F;
+	
+
+	public IEnumerator Do() {
+		yield return new WaitForSeconds(5);    
+		// code to be executed after 5 secs
+	}
+	
+	public void Awake ()
+			{
+				InvokeRepeating ("spawn", 0, Delay);
+			}
+	
+	public void spawn ()
+			{
+			Meteorite.transform.position = new Vector3 (23, Random.Range(YMin,YMax) ,5);
+			Instantiate (Meteorite);
+			}
+		
+}
