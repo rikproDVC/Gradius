01883e6b6aca78d6003ad75056ab93d48601c332
jeremy balk <jeremy.balk@hotmail.com>
2014-06-03 14:32:24 +0200

    Level map with all prefabs,materials,sccripts etc

---
+++ Assets/Levels/Scripts/Powerup.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Powerup : MonoBehaviour {
+	private Transform MyTransform;
+	// Use this for initialization
+	void Start () {
+		MyTransform = transform;	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		if (MyTransform.position.x < -30)
+		{
+			DestroyObject(this.gameObject);
+		}
+	}
+
+	void  OnTriggerEnter2D(Collider2D otherCollider2D)
+	{
+		if (otherCollider2D.tag == "Player") {
+			Destroy(this.gameObject);
+		} 
+
+	}
+
+}
+
--- Assets/Levels/Scripts/Spawn.cs
+++ Assets/Levels/Scripts/Spawn.cs
@@ -8,7 +8,7 @@ public class Spawn : MonoBehaviour {
 	public float Delay= 5F;
 	public float YMin = -4.9F;
 	public float YMax = 4.9F;
-	
+	private Vector3 position;
 
 	public IEnumerator Do() {
 		yield return new WaitForSeconds(5);    
@@ -21,9 +21,18 @@ public class Spawn : MonoBehaviour {
 			}
 	
 	public void spawn ()
-			{
-			Meteorite.transform.position = new Vector3 (23, Random.Range(YMin,YMax) ,5);
-			Instantiate (Meteorite);
+		{
+			if (Meteorite.tag == "Meteorite") {
+				position =new Vector3 (23, Random.Range(YMin,YMax) ,5);
+				Instantiate (Meteorite,position,Quaternion.identity);
 			}
-		
+			if (Meteorite.tag == "Enemy") {
+				position =new Vector3 (23, Random.Range(YMin,YMax) ,1);
+				Instantiate (Meteorite,position,Quaternion.Euler (0, 0, -90));
+			}
+			if (Meteorite.tag == "Powerup") {
+				position =new Vector3 (23, Random.Range(YMin,YMax) ,1);
+				Instantiate (Meteorite);
+			}
+			}
 }
