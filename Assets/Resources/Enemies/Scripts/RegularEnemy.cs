aa14d52f72c6d5183d089f0d871189e7679adf51
rikproDVC <99016312@mydavinci.nl>
2014-06-25 09:20:00 +0200

    Reverted earlier changes to collision detection because of collision errors

--- Assets/Resources/Enemies/Scripts/RegularEnemy.cs
+++ Assets/Resources/Enemies/Scripts/RegularEnemy.cs
@@ -55,8 +55,17 @@ public class RegularEnemy: MonoBehaviour
 
 	// Collision detector...
 	void OnCollisionEnter2D(Collision2D other)
-	{
-
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            Player.PlayerScore -= 100;
+            Destroy(this.gameObject);
+        }
+    }
+    
+    // Collision ...
+    void OnTriggerEnter2D(Collider2D other)
+    {
 		//... with Bullet
 		if(other.transform.tag == "Bullet")
 		{
